9cc690e9ac139198519589cab1a335f9d333ed00
Tu Anh <info@shopbay.vn>
2021-03-29 10:26:59 +0700

    Hotline2

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ if ($fileName == '/') {
 // print_r($fileName); die;
 $file = file_get_contents(".$fileName.html", true);
 $file = str_replace('0903.55.2018', '0908662015', $file);
+$file = str_replace('0976.728.898', '0948896966', $file);
 echo $file;
 
 ?>
